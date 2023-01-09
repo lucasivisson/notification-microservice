aa79f2708cac271054a4e8ae3af90258b4e9f389
Lucas Ivisson <lucasivisson1@gmail.com>
2023-01-09 18:10:32 -0300

    ✨ first commit

---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { SendNotification } from '@application/use-cases/send-notification';
+import { DatabaseModule } from '../database/database.module';
+import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from '@application/use-cases/cancel-notification';
+import { ReadNotification } from '@application/use-cases/read-notification';
+import { UnreadNotification } from '@application/use-cases/unread-notification';
+import { GetRecipientNotification } from '@application/use-cases/get-recipient-notification';
+import { CountRecipientNotification } from '@application/use-cases/count-recipient-notification';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [NotificationsController],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    ReadNotification,
+    UnreadNotification,
+    GetRecipientNotification,
+    CountRecipientNotification,
+  ],
+})
+export class HttpModule {}
