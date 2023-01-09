6a776d7ce20d527ebbb511f41a31a36407a227c6
Lucas Ivisson <lucasivisson1@gmail.com>
2023-01-09 18:14:51 -0300

    ✨ first commit

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,5 +1,5 @@
-import { Module } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
+import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './controllers/notifications.controller';
 import { CancelNotification } from '@application/use-cases/cancel-notification';
